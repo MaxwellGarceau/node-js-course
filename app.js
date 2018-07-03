eae6ef753f082ea87de012dca05f098c29516cc5
Max Garceau <maxwellgarceau@gmail.com>
2018-07-03 09:04:03 -0500

    Getting input from user

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
-// console.log('Starting app');
+console.log('Starting app.js');
 
 const fs = require('fs');
-const os = require('os');
 const _ = require('lodash');
+
 const notes = require('./notes.js');
 
-// console.log(_.isString(true));
-// console.log(_.isString('Max'));
+const command = process.argv[2];
+console.log('Command: ', command);
+console.log('Process.argv', process.argv);
 
-const filteredArray = _.uniq(['Max', 1, 2, 'Max', 3, 4, 1]);
-console.log(filteredArray);
-
-// console.log(notes.add(4, 5));
-
-// let res = notes.addNote();
-// console.log(res);
-
-// let user = os.userInfo();
-
-// fs.appendFileSync('greetings.txt', `Hello, ${user.username}! You are ${notes.age}.`);
+if (command === 'add') {
+  console.log('Adding new note');
+} else if (command === 'list') {
+    console.log('Listing all notes');
+} else if (command === 'read') {
+    console.log('Reading note');
+} else if (command === 'remove') {
+    console.log('Removing note');
+} else {
+  console.log('Command not recognized');
+}
