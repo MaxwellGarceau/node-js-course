9755bcd7e67ae89e9a2839c8e6a30fcb5679f7cd
Max Garceau <maxwellgarceau@gmail.com>
2018-07-06 10:17:22 -0500

    Chrome debugging tools

--- app.js
+++ app.js
@@ -21,7 +21,8 @@ if (command === 'add') {
     console.log('duplicate note');
   }
 } else if (command === 'list') {
-    notes.getAll();
+    const allNotes = notes.getAll();
+    console.log(`Printing: ${allNotes.length} note(s)`);
 } else if (command === 'read') {
     const foundNote = notes.getNote(argv.title);
     console.log(notes.logNote(foundNote));
--- notes.js
+++ notes.js
@@ -31,7 +31,7 @@ const addNote = (title, body) => {
 };
 
 const getAll = () => {
-  console.log('Getting all notes');
+  return fetchNotes();
 };
 
 const getNote = (title) => {
